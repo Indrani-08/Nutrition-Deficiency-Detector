dfab4b10e3146e88809345ce4f62d512cf5f82dd
HA-Akanksha <haakanksha786@gmail.com>
2026-07-29 23:12:41 +0530

    Fix Render port binding

--- Backend/app.py
+++ Backend/app.py
@@ -701,13 +701,11 @@ def predict():
 
 # ---------------- RUN APP ---------------- #
 
+# ---------------- RUN APP ---------------- #
+
 if __name__ == "__main__":
-
     with app.app_context():
         db.create_all()
 
-
-    with app.app_context():
-        db.create_all()
-
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
